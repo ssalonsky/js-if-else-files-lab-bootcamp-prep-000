8d221cb51c045e0f6bb948b24ef0a71f8e63836e
Taylor Salonsky <ssalonsky@gmail.com>
2017-09-20 17:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var name = "Susan"
-var height = "74"
+var name ="Susan"
+var height ="74"
 
-if (name==="Susan") {console.log("The name is correct")}
+if (name === "Susan"){console.log("The name is correct")}
 
-else {console.log("Expected name to be Susan")}
-
-// Don't worry about this
-module.exports = { name, height
+else {console.log("Expected the name to be Susan")
 }
+
+
