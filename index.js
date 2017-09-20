14cbc4e933e63a923c94c3451787205bcebb804e
Taylor Salonsky <ssalonsky@gmail.com>
2017-09-20 18:36:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,4 @@ var height ="70"
 module.exports = { name, height
 }
 
-if (name === "Susan"){console.log("The name is correct")}
 
-else {console.log("Expected the name to be Susan")
-}
-
-if (height === "70"){console.log("The height is correct")}
-
-else {console.log("Expected the height to be 70")}
